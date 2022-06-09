9157b729c2dcefe3ec247e8bd78d2626a9f07c0c
Gabriel Souza <rorato91@gmail.com>
2022-06-09 17:18:22 -0300

    FileViewModel
    Feito a toolbar mais alguns detalhes referente a salvar, abrir e afins

---
+++ WPFNotepad/ViewModels/EditorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFNotepad.Models;
+
+namespace WPFNotepad.ViewModels
+{
+    public class EditorViewModel
+    {
+        public ICommand FormatCommand { get; }
+        public ICommand WrapCommand { get; }
+        public FormatModel Format { get; set; }
+        public DocumentModel Document { get; set; }
+
+        public EditorViewModel(DocumentModel document)
+        {
+            Format = new FormatModel();
+            Document = document;
+            FormatCommand = new RelayCommand(OpenStyleDialog);
+            WrapCommand = new RelayCommand(ToggleWrap);
+        }
+
+        private void OpenStyleDialog()
+        {
+            //Abrir uma janela de estilo
+            throw new NotImplementedException();
+        }
+
+        private void ToggleWrap()
+        {
+            if (Format.Wrap == System.Windows.TextWrapping.Wrap)
+                Format.Wrap = System.Windows.TextWrapping.NoWrap;
+            else
+                Format.Wrap = System.Windows.TextWrapping.Wrap;
+        }
+    }
+}
