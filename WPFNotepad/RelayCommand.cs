00fe84cf6756a501ba4ea62521814606223e687b
Gabriel Souza <rorato91@gmail.com>
2022-06-09 16:51:08 -0300

    Update
    Update em FormatModel mais criação de duas classes

---
+++ WPFNotepad/RelayCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFNotepad
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action _execute;
+        readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+    }
+}
