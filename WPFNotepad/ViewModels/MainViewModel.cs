645927b454943bd4b7d7a78a67e70f72c7f69a89
Gabriel Souza <rorato91@gmail.com>
2022-06-09 17:28:41 -0300

    Save

--- WPFNotepad/ViewModels/MainViewModel.cs
+++ WPFNotepad/ViewModels/MainViewModel.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPFNotepad.Models;
 
 namespace WPFNotepad.ViewModels
 {
-    public class OpenViewModel
+    public class MainViewModel
     {
+        private DocumentModel _document;
+        public EditorViewModel Editor { get; set; }
+        public FileViewModel File { get; set; }
+        public HelpViewModel Help { get; set; }
+
+        public MainViewModel()
+        {
+            _document = new DocumentModel();
+            Help = new HelpViewModel();
+            Editor = new EditorViewModel(_document);
+            File = new FileViewModel(_document);
+        }
     }
 }
