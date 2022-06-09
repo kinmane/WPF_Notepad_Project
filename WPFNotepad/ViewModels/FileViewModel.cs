7bad3179d6d37860ccb5ff243a25ba491c530894
Gabriel Souza <rorato91@gmail.com>
2022-06-09 17:28:30 -0300

    MainViewModel

--- WPFNotepad/ViewModels/FileViewModel.cs
+++ WPFNotepad/ViewModels/FileViewModel.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WPFNotepad.Models;
 
 namespace WPFNotepad.ViewModels
 {
-    internal class FileViewModel
+    public class FileViewModel
     {
+        public DocumentModel Document { get; private set; }
+
+        //Toolbar
+        public ICommand NewCommand { get; }
+        public ICommand SaveCommand { get; }
+        public ICommand SaveAsCommand { get; }
+        public ICommand OpenCommand { get; }
+
+        public FileViewModel(DocumentModel document)
+        {
+            Document = document;
+        }
+
+        public void NewFile()
+        {
+            Document.FilePath = string.Empty;
+            Document.FileName = string.Empty;
+            Document.Text = string.Empty;
+        }
+
+        private void SaveFile()
+        {
+            File.WriteAllText(Document.FilePath, Document.Text);
+        }
+
+        private void SaveFilesAs()
+        {
+            var saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File (*.txt)|*.txt";
+            if(saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, Document.Text);
+            }
+        }
+
+        private void OpenFile()
+        {
+            var openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Document.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void DockFile<T>(T dialog) where T : FileDialog
+        {
+            Document.FilePath = dialog.FileName;
+            Document.FileName = dialog.SafeFileName;
+        }
     }
 }
---
+++ WPFNotepad/ViewModels/HelpViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFNotepad.ViewModels
+{
+    public class HelpViewModel : OberservableObject
+    {
+        public ICommand HelpCommand { get; }
+
+        public HelpViewModel()
+        {
+            HelpCommand = new RelayCommand(null);
+        }
+
+        private void DisplayAbout()
+        {
+            //Abrir tela de ajuda
+
+        }
+    }
+}
